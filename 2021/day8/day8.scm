15d95927ae2476eae14dd1084aa9873bbc3557bc
Thomas White <taw@physics.org>
2021-12-09 16:58:19 +0100

    Day 8 part 2 (first attempt)

--- 2021/day8/day8.scm
+++ 2021/day8/day8.scm
@@ -1,6 +1,7 @@
 (use-modules
   (ice-9 textual-ports)
   (ice-9 receive)
+  (srfi srfi-26)
   (srfi srfi-1))
 
 
@@ -46,8 +47,56 @@
           outputs)))
 
 
+(define (make-segment-table signals)
+  (let ((table (make-hash-table 7)))
+    (for-each
+      (lambda (c)
+        (hashq-set! table c '(A B C D E F G)))
+      (apply append (map string->list signals)))
+    table))
+
+
+(define (filter-segments table signal segments)
+  (hashq-set! table
+              signal
+              (lset-intersection eq? segments
+                                 (hashq-ref table signal))))
+
+
+(define (filter-segments-by-len signal-patterns
+                                segment-table
+                                signal-length
+                                segments-involved)
+  (for-each
+    (cut filter-segments segment-table <> segments-involved)
+    (string->list
+      (find
+        (lambda (s)
+          (eq? signal-length
+               (string-length s)))
+        signal-patterns))))
+
+
+(define (print-hash-table ht)
+  (hash-for-each (lambda (key value)
+                   (display key)
+                   (display " ---> ")
+                   (display value)
+                   (newline))
+                 ht))
+
+
+(define (decode-display signal-patterns digits)
+  (let ((segment-table (make-segment-table signal-patterns)))
+    (filter-segments-by-len signal-patterns segment-table 2 '(C F))
+    (filter-segments-by-len signal-patterns segment-table 3 '(A C F))
+    (filter-segments-by-len signal-patterns segment-table 4 '(B C D F))
+    (filter-segments-by-len signal-patterns segment-table 7 '(A B C D F G))
+    segment-table))
+
+
 (define (part2)
   (receive
-    (signal-patterns outputs)
+    (signal-patterns digits)
     (call-with-input-file "example-input" read-input)
-    ))
+    (map decode-display signal-patterns digits)))

39d6cc971c635c03c9a56ab672305254c63040f0
Thomas White <taw@physics.org>
2023-01-12 17:44:09 +0100

    Day 21 part 2

--- 2022/day21-monkey-math/day21.jl
+++ 2022/day21-monkey-math/day21.jl
@@ -2,11 +2,21 @@
 
 abstract type Monkey end
 
+struct HumanMonkey <: Monkey
+    val
+end
+
 struct NumMonkey <: Monkey
     val
 end
 
 struct MathMonkey <: Monkey
+    op
+    v1::Monkey
+    v2::Monkey
+end
+
+struct RootMonkey <: Monkey
     op
     v1::Monkey
     v2::Monkey
@@ -30,7 +40,11 @@ end
 
 function parsemonkey(name, lines)
     l = split(lines[name])
-    if length(l) == 1
+    if name == "humn"
+        HumanMonkey(parse(Int, l[1]))
+    elseif name == "root"
+        RootMonkey(op(l[2]), parsemonkey(l[1], lines), parsemonkey(l[3], lines))
+    elseif length(l) == 1
         NumMonkey(parse(Int, l[1]))
     else
         MathMonkey(op(l[2]), parsemonkey(l[1], lines), parsemonkey(l[3], lines))
@@ -49,10 +63,56 @@ end
 
 
 eval_monkey(m::NumMonkey) = m.val
+eval_monkey(m::HumanMonkey) = m.val
 function eval_monkey(m::MathMonkey)
     m.op(eval_monkey(m.v1), eval_monkey(m.v2))
 end
+function eval_monkey(m::RootMonkey)
+    m.op(eval_monkey(m.v1), eval_monkey(m.v2))
+end
+
+
+has_human(m::HumanMonkey) = true
+has_human(m::NumMonkey) = false
+function has_human(m::MathMonkey)
+    has_human(m.v1) || has_human(m.v2)
+end
+
+find_human_val(m::HumanMonkey, n) = n
+
+function find_human_val(m::RootMonkey)
+    if has_human(m.v1)
+        find_human_val(m.v1, eval_monkey(m.v2))
+    else
+        find_human_val(m.v2, eval_monkey(m.v1))
+    end
+end
+
+function find_human_val(m::MathMonkey, n)
+    if has_human(m.v1)
+        if m.op == +
+            find_human_val(m.v1, n-eval_monkey(m.v2))
+        elseif m.op == -
+            find_human_val(m.v1, n+eval_monkey(m.v2))
+        elseif m.op == *
+            find_human_val(m.v1, n÷eval_monkey(m.v2))
+        elseif m.op == ÷
+            find_human_val(m.v1, n*eval_monkey(m.v2))
+        end
+    else
+        if m.op == +
+            find_human_val(m.v2, n-eval_monkey(m.v1))
+        elseif m.op == -
+            find_human_val(m.v2, eval_monkey(m.v1)-n)
+        elseif m.op == *
+            find_human_val(m.v2, n÷eval_monkey(m.v1))
+        elseif m.op == ÷
+            find_human_val(m.v2, eval_monkey(m.v1)÷n)
+        end
+    end
+end
+
 
 root = readmonkeys("input")
 println("Part 1: ", eval_monkey(root))
-println("Part 2: ", eval_monkey(root))
+println("Part 2: ", find_human_val(root))
